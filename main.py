f4595bc41d75546ea0d0064c181337066927e69f
Ibrahim Khan <129583293+amikraa@users.noreply.github.com>
2025-09-27 00:43:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 app = FastAPI()
 
 # ---------------- CONFIG ----------------
-MAINTENANCE_MODE = False   # 🔴 set True to pause service
+MAINTENANCE_MODE = True   # 🔴 set True to pause service
 FAMPAY_TOKEN = "eyJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiZXBrIjp7Imt0eSI6Ik9LUCIsImNydiI6Ilg0NDgiLCJ4IjoiS0s1Sk9zbjRUNGF3NF9EOVd6Z0U0QkJQRF9leGZkZkU4QTdxNXM5QlJOS0I0T2p5SXJqVGMxdnh3RHlBREJoQXpYbjVNd2M5aWU4In0sImFsZyI6IkVDREgtRVMifQ..w3b_31d31e1ptAh06UxMEg.2D0FKvzqMRRmjP35UjJYUt4Ow8yOEOOmyRE6MX-xd9a-sJJK9X0G_OyAL0lBGqTHGz6eixM6I6Ji7pvdO8AsL9QW4O7hxOUAWjEC4Yp23kHeKHrxWcfxAPa6wUmstMGGt7_yvmv7YrhdB_XzFIbITgJhIMlLer1PPMqD_voQ9LU3dUMeq6vUkIVxtDGwIOV1ys2sUqi5JZ6v9KkeejjjHn2_wp6qjAlARKHSbY0-LOgfb6HHdGcKVD7Wo_jhKA7Ez322K_REX7PwZxovutfpUw.HLBRzIVXrbqOrgRdiveiRCYOyPZgKrq9gEt_GkUTS9U"
 API_VERIFY = "https://halfblood.famapp.in/vpa/verifyExt"
 API_PAYOUT = "https://westeros.famapp.in/txn/create/payout/add/"
